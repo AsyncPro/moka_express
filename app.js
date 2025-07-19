3bfaedca73115d70d1edc4ad8807f9c98bbb24bb
AsyncPro <async_pro@outlook.com>
2025-07-19 13:16:44 -0600

    con el server

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+
+
+
 import express from "express";
+import dotenv from "dotenv";
 import Root_router from "./src/routes/root.router.js";
 
+dotenv.config();
+
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "localhost";
 
 app.use(express.json());
 app.use("/api", Root_router);
 
-const PORT = 4500;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.json({ app: "funcionando" });
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`🚀 Servidor corriendo en http://${HOST}:${PORT}`);
 });
